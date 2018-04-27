bbf1c01003509f83b3b4dc6d96cab3558cf66c2d
Austin Wagner <austin.e.wagner@gmail.com>
2018-04-26 20:33:43 -0700

    Spotify working

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 const twitterKeys = require('./keys');
 const request = require('request');
 const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
 
 const action = process.argv[2];
 
@@ -24,4 +25,27 @@ if (action === 'my-tweets') {
 
         }
     });
+} else if (action === 'spotify-this-song') {
+    const song = process.argv[3];
+    let spotify = new Spotify({
+        id: 'd3b691929cf04f92a44cb221690df275',
+        secret: '6b3e1468353f48a9be6f84d0fc761ea2'
+    });
+
+    spotify.search({ type: 'track', query: 'All the Small Things', limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        //Artist
+        console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+        //Song
+        console.log(`Title: ${data.tracks.items[0].name}`);
+        //Preview Link
+        console.log(`Link: ${data.tracks.items[0].external_urls.spotify}`);
+        //Album
+        console.log(`Album: ${data.tracks.items[0].album.name}`);        
+    });
+
+
 }
